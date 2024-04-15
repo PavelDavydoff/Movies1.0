97c09fea1de0e42dd0f5be1c6667abea7948a56b
Pavel_Davydov <david20506@yandex.ru>
2024-04-15 12:08:19 +0500

    Clean Architecture MVC

--- app/src/main/java/com/example/movies10/Creator.kt
+++ app/src/main/java/com/example/movies10/Creator.kt
@@ -1,10 +1,14 @@
 package com.example.movies10
 
+import android.app.Activity
 import com.example.movies10.data.MoviesRepositoryImpl
 import com.example.movies10.data.network.RetrofitNetworkClient
 import com.example.movies10.domain.api.MoviesInteractor
 import com.example.movies10.domain.api.MoviesRepository
 import com.example.movies10.domain.impl.MoviesInteractorImpl
+import com.example.movies10.presentation.MoviesSearchController
+import com.example.movies10.presentation.PosterController
+import com.example.movies10.ui.movies.MoviesAdapter
 
 object Creator {
     private fun getMoviesRepository(): MoviesRepository {
@@ -13,5 +17,16 @@ object Creator {
 
     fun provideMoviesInteractor(): MoviesInteractor {
         return MoviesInteractorImpl(getMoviesRepository())
+    }
+
+    fun provideMoviesSearchController(
+        activity: Activity,
+        adapter: MoviesAdapter
+    ): MoviesSearchController {
+        return MoviesSearchController(activity, adapter)
+    }
+
+    fun providePosterController(activity: Activity): PosterController {
+        return PosterController(activity)
     }
 }
